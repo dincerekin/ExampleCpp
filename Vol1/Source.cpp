65a93c8d2460ec8c4aa3543ddcda52bedefceef8
Ekin Dincer <ekindincer@hotmail.com>
2020-04-15 18:09:39 +0800

    Add project files.

---
+++ Vol1/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// Outputting to console.
+	printf("Hello World!\n"); // C style
+	cout << "Hello World!" << endl; // C++ style
+
+	return 0;
+}
