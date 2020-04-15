9562de7ab507483c4b5d28fc0f60703d08f06764
Ekin Dincer <ekindincer@hotmail.com>
2020-04-15 19:16:49 +0800

    added control flow statements

--- Vol1/Source.cpp
+++ Vol1/Source.cpp
@@ -6,7 +6,37 @@ using namespace std;
 int main() {
 	// Outputting to console.
 	printf("Hello World!\n"); // C style
-	cout << "Hello World!" << endl; // C++ style
+	cout << "Hello " << "World!" << endl; // C++ style
+
+	// Declaring variable.
+	int myAge = 69;
+
+	cout << myAge << endl;
+	cout << "sizeof(integer)" << sizeof(myAge) << endl;
+
+	//Data types
+	//short, int, float, double, bool, char, string, void,
+
+	//Typecasting
+	cout << "4 / 5 = " << 4 / 5 << endl;
+	cout << "(float) 4 / 5 = " << (float) 4 / 5 << endl;
+
+
+
+//CONTROL FLOW
+	//If statements
+	if (true) {}
+	else if(false){}
+	else{}
+
+	//Switch statements
+	switch (myAge) {
+	case 10: cout << "myAge is 10!" << endl; break;
+	case 20: cout << "myAge is 20!" << endl; break;
+	case 69: cout << "myAge is 69!" << endl; break;
+	default: cout << "myAge is not listed!" << endl; break;
+	}
+
 
 	return 0;
 }
