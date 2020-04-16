7ac5d91fe845ccefded63bf5e5c4a19d1c388c72
Ekin Dincer <ekindincer@hotmail.com>
2020-04-17 02:39:12 +0800

    update

--- Vol1/ExampleCpp.cpp
+++ Vol1/ExampleCpp.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Function prototypes. These are used so the meat of functions can be placed after main().
 int addNumbers(int, int);
-int addNumbers(int,int,int);
+int addNumbers(int, int, int);
 int getFactorial(int);
 void makeMeYoung(int*);
 
@@ -27,11 +27,11 @@ int main() {
 	cout << "5-- = " << five-- << endl;
 	cout << "--5 = " << --five << endl << endl;
 
-		// Shorthand assignment operators
-			// a += b is equivalent to a = a + b
-			// There is also -=, *=, /=, %=
+	// Shorthand assignment operators
+		// a += b is equivalent to a = a + b
+		// There is also -=, *=, /=, %=
 
-		// Outputting to console.
+	// Outputting to console.
 	printf("Hello World!\n"); // C style
 	cout << "Hello " << "World!" << endl << endl; // C++ style, use this.
 
@@ -40,12 +40,12 @@ int main() {
 	cout << "myAge = " << myAge << endl;
 	cout << "sizeof(myAge) = " << sizeof(myAge) << endl << endl;
 
-		// Data types:
-			// short, long, int, float, double, bool, char, string, void.
-		// Data type modifiers:
-			// unsigned, const, static
+	// Data types:
+		// short, long, int, float, double, bool, char, string, void.
+	// Data type modifiers:
+		// unsigned, const, static
 
-		// Setting integers in hexadecimal.
+	// Setting integers in hexadecimal.
 	int hex_integer = 0xFF;
 	cout << "hex_integer: " << hex_integer << endl;
 	cout << "hex_integer (std::hex)" << std::hex << hex_integer << endl; // Displaying integers in hexadecimal format.
@@ -56,7 +56,7 @@ int main() {
 	cout << "4 / 5 = " << 4 / 5 << endl;
 	cout << "(float) 4 / 5 = " << (float)4 / 5 << endl << endl;
 
-		// User input.
+	// User input.
 	string yourName;
 	cout << "What is your name? ";
 	getline(cin, yourName);
@@ -67,11 +67,11 @@ int main() {
 	int myFavNums[5] = { 4, 13, 14, 24, 34 };
 	int myFavNumsMulti[3][3] = { {4, 13, 14}, {4, 13, 14},{14, 24, 34 } };
 
-		// Indexing.
+	// Indexing.
 	cout << "myFavNums[0] = " << myFavNums[0] << endl;
 	cout << "myFavNumsMulti[0] = " << myFavNumsMulti[0][0] << endl;
 
-		// Assigning
+	// Assigning
 	myFavNums[0] = 69;
 	myFavNumsMulti[0][0] = 69;
 	cout << "myFavNums[0] = " << myFavNums[0] << endl;
@@ -83,7 +83,7 @@ int main() {
 	else if (false) {}
 	else {}
 
-		// Switch statement.
+	// Switch statement.
 	switch (myAge) {
 	case 10: cout << "myAge is 10!" << endl; break;
 	case 20: cout << "myAge is 20!" << endl; break;
@@ -93,19 +93,19 @@ int main() {
 
 	cout << endl;
 
-		// Conditional assignment.
+	// Conditional assignment.
 	bool logical_statement = true;
 	string value_if_true = "true";
 	string value_if_false = "false";
 	string condional_assignment = logical_statement ? value_if_true : value_if_false;
 
-		// For statement.
+	// For statement.
 	for (int i = 0; i <= 10; ++i) { //use preincrement (++i) over post-incrment (i++) in for loops).
 		continue; //skip to next iteration of loop.
 		break; //break out of loop.
 	}
 
-		// While statement.
+	// While statement.
 	int condition = 0;
 	while (condition < 5) {
 		cout << "condition = " << condition << endl;
@@ -114,7 +114,7 @@ int main() {
 
 	cout << endl;
 
-		// Do-while statement.
+	// Do-while statement.
 	condition = 0;
 	do {
 		cout << "condition = " << condition << endl;
@@ -141,10 +141,10 @@ int main() {
 		// We can read and write to files using text or machine readable binary.
 	string steveQuote = "A day without sunshine is like, you know, night";
 
-		// Create an output filestream and if the file doesn't exist create it.
+	// Create an output filestream and if the file doesn't exist create it.
 	ofstream writer("stevequote.txt");
 
-		// Verify that the file stream object was created.
+	// Verify that the file stream object was created.
 	if (!writer) {
 		cout << "Error opening file" << endl;
 		// Signal that an error occurred.
@@ -158,11 +158,11 @@ int main() {
 		writer.close();
 	}
 
-		// Open a stream to append to whats there with ios::app
-		// ios::binary : Treat the file as binary
-		// ios::in : Open a file to read input
-		// ios::trunc : Default
-		// ios::out : Open a file to write output
+	// Open a stream to append to whats there with ios::app
+	// ios::binary : Treat the file as binary
+	// ios::in : Open a file to read input
+	// ios::trunc : Default
+	// ios::out : Open a file to write output
 	ofstream writer2("stevequote.txt", ios::app);
 
 	if (!writer2) {
@@ -176,7 +176,7 @@ int main() {
 	}
 	char letter;
 
-		// Read characters from a file using an input file stream.
+	// Read characters from a file using an input file stream.
 	ifstream reader("stevequote.txt");
 
 	if (!reader) {
@@ -224,7 +224,6 @@ int getFactorial(int number) {
 	return sum;
 }
 
-
 void makeMeYoung(int* age) { // Since I'm getting a pointer use int*.
 	cout << "I used to be " << *age << endl; // Refer to the referenced variable with *age.
 	*age = 21;
@@ -239,16 +238,16 @@ void actYourAge(int& age) {
 	// TODO: Class.
 
 class Animal {
-	// private variables are only available to methods in the class
+	// Private variables are only available to methods in the class.
 private:
 	int height;
 	int weight;
 	string name;
 
-	// A static variable shares the same value with every object in the class
+	// A static variable shares the same value with every object in the class.
 	static int numOfAnimals;
 
-	// Public variables can be accessed by anything with access to the object
+	// Public variables can be accessed by anything with access to the object.
 public:
 	int getHeight() { return height; }
 	int getWeight() { return weight; }
@@ -257,60 +256,91 @@ public:
 	void setWeight(int kg) { weight = kg; }
 	void setName(string dogName) { name = dogName; }
 
-	// Declared as a prototype
+	// Declared as a prototype.
 	void setAll(int, int, string);
 
-	// Declare the constructor
+	// Declare the constructor.
 	Animal(int, int, string);
 
-	// Declare the deconstructor
+	// Declare the deconstructor.
 	~Animal();
 
-	// An overloaded constructor called when no data is passed
+	// An overloaded constructor called when no data is passed.
 	Animal();
 
-	// protected members are available to members of the same class and
-	// sub classes
+	// Protected members are available to members of the same class and sub classes.
 
-	// Static methods aren't attached to an object and can only access
-	// static member variables
+	// Static methods aren't attached to an object and can only access static member variables.
 	static int getNumOfAnimals() { return numOfAnimals; }
 
-	// This method will be overwritten in Dog
+	// This method will be overwritten in Dog.
 	void toString();
 };
 
 int Animal::numOfAnimals = 0;
 
 void Animal::setAll(int height, int weight, string name) {
-	// This is used to refer to an object created of this class type
+	// This is used to refer to an object created of this class type.
 	this->height = height;
 	this->weight = weight;
 	this->name = name;
 	Animal::numOfAnimals++;
 }
 
-// A constructor is called when an object is created
+	// A constructor is called when an object is created.
 Animal::Animal(int height, int weight, string name) {
 	this->height = height;
 	this->weight = weight;
 	this->name = name;
 }
 
-// A constructor called when no attributes are passed
+	// A constructor called when no attributes are passed.
 Animal::Animal() {
 	this->height = 0;
 	this->weight = 0;
 	numOfAnimals++;
 }
 
-// The destructor is called when an object is destroyed
+	// The destructor is called when an object is destroyed.
 Animal::~Animal() {
 	cout << "Animal " << this->name << " destroyed" << endl;
 }
 
-// This method prints object info to screen and will be overwritten
+	// This method prints object info to screen and will be overwritten.
 void Animal::toString() {
 	cout << this->name << " is " << this->height << " cms tall and "
 		<< this->weight << " kgs in weight" << endl;
+}
+
+	// We can inherit the variables and methods of other classes.
+class Dog : public Animal {
+private:
+	string sound = "WOOF!!";
+public:
+	void getSound() { cout << sound << endl; }
+
+	// Declare the constructor.
+	Dog(int, int, string, string);
+
+	// Declare the default constructor and call the default superclass constructor.
+	Dog() : Animal() {};
+
+	// Overwrite toString.
+	void toString();
+};
+
+// Dog constructor passes the right attributes to the superclass
+// constructor and then handles the attribute bark that remains
+Dog::Dog(int height, int weight, string name, string bark) :
+	Animal(height, weight, name) {
+	this->sound = bark;
+}
+
+// toString method overwritten
+void Dog::toString() {
+	// Because the attributes were private in Animal they must be retrieved
+	// by called the get methods
+	cout << this->getName() << " is " << this->getHeight() <<
+		" cms tall and " << this->getWeight() << " kgs in weight and says " <<
+		this->sound << endl;
 }
